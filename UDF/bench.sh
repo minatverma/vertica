eccc7c83e79b7efeb2de700cfd1feeabf873d265
Daniel <daniel@twingo.co.il>
2012-02-15 00:56:27 +0200

    benchmark script

---
+++ UDF/bench.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+VSQL=/opt/vertica/bin/vsql
+
+# compile MonthName.cpp
+g++ -D HAVE_LONG_INT_64 -I /opt/vertica/sdk/include -Wall -shared -Wno-unused-value \
+	-fPIC -o MonthNameLib.so MonthName.cpp /opt/vertica/sdk/include/Vertica.cpp
+
+# create random data
+echo -e "import random\n\nfor i in xrange(1000000):\n\tprint i\n\n" | \
+	python > /tmp/int_1mil.dat
+
+# create table
+$VSQL -c "create table if not exists UDFSimpleBencmark ( m_num int )"
+
+# load data
+$VSQL -c "copy UDFSimpleBencmark from '/tmp/int_1mil.dat' direct"
+
+# measurement
+echo -e "SQL month name DECODE bench:\n"
+echo -e "\\\timing\n\\o /dev/null\nselect month_name_decode(m_num) from UDFSimpleBencmark;" | \
+	$VSQL | sed 's/^Time:.\+All rows formatted: \([^ ]\+\).\+/\1/'
+
+echo -e "\n---\n"
+
+echo -e "SQL month name CASE bench:\n"
+echo -e "\\\timing\n\\o /dev/null\nselect month_name_case(m_num) from UDFSimpleBencmark;" | \
+	        $VSQL | sed 's/^Time:.\+All rows formatted: \([^ ]\+\).\+/\1/'
+
+echo -e "\n---\n"
+
+
+echo -e "SQL month name CPP bench:\n"
+echo -e "\\\timing\n\\o /dev/null\nselect month_name_cpp(m_num) from UDFSimpleBencmark;" | \
+	                $VSQL | sed 's/^Time:.\+All rows formatted: \([^ ]\+\).\+/\1/'
+echo -e "\n---\n"
+
+$VSQL -c "drop table if exists UDFSimpleBencmark"
+#rm -f /tmp/int_1mil.dat
+
+
