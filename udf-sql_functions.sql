4b4dafcd4ed0871134134a09779567372fd35049
Danik <daniel@twingo.co.il>
2012-06-13 10:12:10 +0300

    update -> commit

---
+++ udf-sql_functions.sql
@@ -0,0 +1,40 @@
+
+------ 
+-- Returns if argument is from type INT. 
+-- @a      - int candidate
+-- @return - true iff argument is INT
+------
+CREATE OR REPLACE FUNCTION isInt(a varchar(65000))
+RETURN BOOLEAN
+  AS BEGIN
+    RETURN REGEXP_LIKE(a,'^\d{1,19}$') OR (
+           REGEXP_LIKE(a,'^[1-9](\d+)?\.\d+e[-+]\d+') AND  
+           ABS(a::numeric) <= 9223372036854775807);
+  END;
+
+
+------ 
+-- Converts month number to name 
+-- @month  - moth number candidate
+-- @return - month name iff month number is valid otherwise null
+------
+
+CREATE OR REPLACE FUNCTION month_name(month INT)
+RETURN VARCHAR(10)
+  AS BEGIN
+    RETURN (CASE
+      WHEN (month = 1)  THEN 'January'
+      WHEN (month = 2)  THEN 'February'
+      WHEN (month = 3)  THEN 'March'
+      WHEN (month = 4)  THEN 'April'
+      WHEN (month = 5)  THEN 'May'
+      WHEN (month = 6)  THEN 'June'
+      WHEN (month = 7)  THEN 'July'
+      WHEN (month = 8)  THEN 'August'
+      WHEN (month = 9)  THEN 'September'
+      WHEN (month = 10) THEN 'October'
+      WHEN (month = 11) THEN 'November'
+      WHEN (month = 12) THEN 'December'
+    END);
+  END;
+
