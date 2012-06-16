1c67e7a6de7c09531598fe21ed5d82198c235b96
Daniel <daniel@twingo.co.il>
2012-06-16 17:54:16 +0300

    commit

--- UDF/bench.sh
+++ UDF/bench.sh
@@ -14,27 +14,50 @@ RFUNC='month_name_rfunc'
 
 ## abort function - more readable
 ## [ $? -ne 0 ] && echo 'ERROR' 2>&1 && exit 1
-function abort_if {
+function abort_if 
+{
     if [ $? -ne 0 ]; then
         echo `date +'[ %H:%M:%S ]'`'[ ERROR ] '$1 2>&1
         exit 1
     fi      
-    return 0
 }
 
-function test_function {
+
+## since I do avg I don't use in Bash::time or /sbin/time
+function test_function 
+{
+    sql_query='select '"$1"'(m_num) from UDFSimpleBencmark;'
+    
     start_time=`date +'%s.%N'`
-    SQL_QUERY='select '"$1"'(m_num) from UDFSimpleBencmark;'
-    $VSQL -c '"'${SQL_QUERY}'"' >/dev/null
+    $VSQL -c '"'${sql_query}'"' >/dev/null
     end_time=`date +'%s.%N'`
-    execution_time=`echo ${end_time} - ${start_time} | bc`
-    return $execution_time
+    
+    echo ${end_time} - ${start_time} | bc
+}
+
+
+function test_avg 
+{
+    echo -e "TEST function : "${1}"\n"
+    avg=0
+    for i in `seq 1 $ATTEMPS`;do
+        avg=${avg}+`test_func ${1}`
+    done
+    avg=`echo ${avg} | sed -e 's/\(+[0-9]\+.[0-9][0-9]\)[0-9]\+/\1/g' -e 's/^0+//'`
+    echo -e "Execution time for ${ATTEMPS} :\n"
+    for attemp in `echo ${avg} | sed 's/+/\n/g'`; do
+        echo $attemp
+    done
+    echo -e "-------------\n"
+    echo -e ${avg} / 5 | bc
 }
 
 ## create cpp library
-g++ -D HAVE_LONG_INT_64 -I /opt/vertica/sdk/include -Wall -shared -Wno-unused-value \
-	-fPIC -o MonthNameLib.so MonthName.cpp /opt/vertica/sdk/include/Vertica.cpp
-abort_if 'compilation failed'
+g++ -D HAVE_LONG_INT_64                         \ 
+	-I /opt/vertica/sdk/include             \
+	-Wall -shared -Wno-unused-value -fPIC   \
+	-o MonthNameLib.so MonthName.cpp /opt/vertica/sdk/include/Vertica.cpp
+abort_if 'failed to compile'
 
 
 ## load library to Vertica 
@@ -64,31 +87,9 @@ $VSQL -c "copy UDFSimpleBencmark from '"${DATA}"' direct;"
 abort_if 'failed load data to Vertica'
 
 
-## 
-echo -e "SQL month name DECODE bench:\n"
-avg=0
-for i in `seq 1 $ATTEMPS`;do
-	avg=$avg+`test_func ${DEC_FUNC}`
-done
-avg=`echo ${avg} / 5 | bc`
-
-
-echo -e "\\\timing\n\\o /dev/null\nselect month_name_decode(m_num) from UDFSimpleBencmark;" | \
-	$VSQL | sed 's/^Time:.\+All rows formatted: \([^ ]\+\).\+/\1/'
-
-echo -e "\n---\n"
-
-echo -e "SQL month name CASE bench:\n"
-echo -e "\\\timing\n\\o /dev/null\nselect month_name_case(m_num) from UDFSimpleBencmark;" | \
-	        $VSQL | sed 's/^Time:.\+All rows formatted: \([^ ]\+\).\+/\1/'
-
-echo -e "\n---\n"
-
-
-echo -e "SQL month name CPP bench:\n"
-echo -e "\\\timing\n\\o /dev/null\nselect month_name_cpp(m_num) from UDFSimpleBencmark;" | \
-	                $VSQL | sed 's/^Time:.\+All rows formatted: \([^ ]\+\).\+/\1/'
-echo -e "\n---\n"
+test_avg ${CPP_FUNC}
+test_avg ${DEC_FUNC}
+test_avg ${CASE_FUNC}
 
 $VSQL -c "drop table if exists UDFSimpleBencmark"
 rm -f /tmp/int_1mil.dat
